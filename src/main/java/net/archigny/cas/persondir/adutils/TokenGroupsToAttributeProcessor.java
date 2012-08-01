6d88ccd6a8a33abfb615ad847141b9b42a2b46b5
Philippe MARASSE <philippe.marasse@laposte.net>
2012-08-02 00:19:01 +0200

    Updated to cas-persondir-ldap 0.1.7 with user direct dn fetching dropped dependancy upon spring-ldap.

--- src/main/java/net/archigny/cas/persondir/adutils/TokenGroupsToAttributeProcessor.java
+++ src/main/java/net/archigny/cas/persondir/adutils/TokenGroupsToAttributeProcessor.java
@@ -1,25 +1,15 @@
 package net.archigny.cas.persondir.adutils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.naming.InvalidNameException;
-import javax.naming.Name;
-import javax.naming.ldap.LdapName;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.ldap.core.ContextMapper;
-import org.springframework.ldap.core.ContextSource;
-import org.springframework.ldap.core.DirContextAdapter;
-import org.springframework.ldap.core.LdapTemplate;
-
 import net.archigny.cas.persondir.processors.IAttributesProcessor;
 import net.archigny.utils.ad.api.IActiveDirectoryTokenGroupsRegistry;
 
@@ -29,34 +19,6 @@ public class TokenGroupsToAttributeProcessor implements IAttributesProcessor, In
                                                                             .getLogger(TokenGroupsToAttributeProcessor.class);
 
     /**
-     * Attribute to retrieve from AD with global catalog enabled. According to LDAPv3, ;binary is appended to retrieve data as a
-     * byte[] instead of a String
-     * 
-     * @see http://msdn.microsoft.com/en-us/library/cc223395%28v=prot.13%29
-     * @see http://docs.oracle.com/javase/jndi/tutorial/ldap/misc/attrs.html
-     */
-    private static String[]             TOKEN_GROUPS_ATTRS          = { "tokenGroups;binary" };
-
-    /**
-     * Attribute name as send by spring-ldap (note that ;binary still exists in attribute name !)
-     */
-    private static String               TOKEN_GROUPS_ATTR           = "tokenGroups;binary";
-
-    /**
-     * Attribute to retrieve from AD with or without global catalog enabled. According to LDAPv3, ;binary is appended to retrieve
-     * data as a byte[] instead of a String
-     * 
-     * @see http://msdn.microsoft.com/en-us/library/cc223395%28v=prot.13%29
-     * @see http://docs.oracle.com/javase/jndi/tutorial/ldap/misc/attrs.html
-     */
-    private static String[]             TOKEN_GROUPS_NO_GC_ATTRS    = { "tokenGroupsNoGCAcceptable;binary" };
-
-    /**
-     * Attribute name as send by spring-ldap (note that ;binary still exists in attribute name !)
-     */
-    private static String               TOKEN_GROUPS_NO_GC_ATTR     = "tokenGroupsNoGCAcceptable;binary";
-
-    /**
      * tokenGroups registry used to translate SID to ldap groups
      */
     IActiveDirectoryTokenGroupsRegistry tokenRegistry;
@@ -64,91 +26,53 @@ public class TokenGroupsToAttributeProcessor implements IAttributesProcessor, In
     /**
      * source attribute containing group tokens in byte[] or String form
      */
-    private String                      dnAttribute;
+    private String                      sourceAttribute;
 
     /**
      * target attributes containing decoded groups
      */
     private String                      targetAtribute;
 
-    /**
-     * Specify if an IllegalFormatException will be raised if input data of UUID is invalid (eg not 128bit long)
-     */
-    private boolean                     raiseIllegalFormatException = false;
-
-    /**
-     * True : tokenGroups attribute should be used, False : tokenGroupsNoGCAcceptable is used
-     */
-    private boolean                     useTokenGroupsAttribute     = true;
-
-    /**
-     * LDAP Template used to retrieve tokenGroups
-     */
-    private LdapTemplate                ldapTemplate;
-
-    /**
-     * LDAP Context Source
-     */
-    private ContextSource               contextSource;
-
-    /**
-     * Base DN used by Ldap Context Source (cannot fetch it from "ContextSource"...)
-     */
-    private LdapName                    contextSourceBaseDN;
-
     @Override
     public void afterPropertiesSet() throws Exception {
 
-        if (contextSource == null) {
-            throw new BeanCreationException("contextSrouce cannot be null");
-        }
         if (tokenRegistry == null) {
             throw new BeanCreationException("tokenRegistry cannot be null");
         }
-        if (dnAttribute == null) {
+        if (sourceAttribute == null) {
             throw new BeanCreationException("dnAttribute cannot be null");
         }
         if (targetAtribute == null) {
             throw new BeanCreationException("targetAttribute cannot be null");
         }
 
-        ldapTemplate = new LdapTemplate(contextSource);
-        ldapTemplate.setIgnorePartialResultException(true);
-        ldapTemplate.setIgnoreNameNotFoundException(true);
-
     }
 
     @Override
     public void processAttributes(Map<String, List<Object>> attributes) {
 
-        List<Object> dnValues = attributes.get(dnAttribute);
+        List<Object> sourceValues = attributes.get(sourceAttribute);
 
         // Source attribute found ?
-        if ((dnValues == null) || (dnValues.isEmpty())) {
+        if ((sourceValues == null) || (sourceValues.isEmpty())) {
             return;
         }
 
         if (log.isDebugEnabled()) {
-            log.debug("DN value found : [" + dnValues.get(0) + "] processing...");
+            log.debug(sourceValues.size() + " token values found, processing...");
         }
 
         try {
-            Name userDN = new LdapName((String) dnValues.get(0));
+            List<Object> targetValues = new ArrayList<Object>(sourceValues.size());
 
-            if (contextSourceBaseDN != null) {
-                if (userDN.startsWith(contextSourceBaseDN)) {
-
-                    // Get rid of base DN before querying the directory
-                    userDN = userDN.getSuffix(contextSourceBaseDN.size());
-                    if (log.isDebugEnabled()) {
-                        log.debug("base DN of contextSource found as suffix of userDN. deleting, new userDN : " + userDN.toString());
-                    }
+            for (Object rawValue : sourceValues) {
+                String groupName = tokenRegistry.getDnFromToken((byte[]) rawValue);
+                if (groupName == null) {
+                    log.info("unable to decode raw tokebGroup class : " + rawValue.getClass().getCanonicalName());
+                } else {
+                    targetValues.add(groupName);
                 }
             }
-
-            @SuppressWarnings("unchecked")
-            List<Object> targetValues = (List<Object>) ldapTemplate.lookup(userDN, (useTokenGroupsAttribute ? TOKEN_GROUPS_ATTRS
-                    : TOKEN_GROUPS_NO_GC_ATTRS), new TokenGroupMapper());
 
             if ((targetValues != null) && (!targetValues.isEmpty())) {
                 attributes.put(targetAtribute, targetValues);
@@ -156,8 +80,8 @@ public class TokenGroupsToAttributeProcessor implements IAttributesProcessor, In
                 log.info("This DN seems to have no tokenGroups");
             }
 
-        } catch (InvalidNameException e1) {
-            log.error("Unable to build LdapName from " + dnValues.get(0));
+        } catch (ClassCastException e1) {
+            log.error("Unable to cast a source attribute value to byte[]");
         }
 
     }
@@ -183,14 +107,14 @@ public class TokenGroupsToAttributeProcessor implements IAttributesProcessor, In
         this.tokenRegistry = tokenRegistry;
     }
 
-    public String getDnAttribute() {
+    public String getSourceAttribute() {
 
-        return dnAttribute;
+        return sourceAttribute;
     }
 
-    public void setDnAttribute(String dnAttribute) {
+    public void setSourceAttribute(String dnAttribute) {
 
-        this.dnAttribute = dnAttribute;
+        this.sourceAttribute = dnAttribute;
     }
 
     public String getTargetAtribute() {
@@ -201,103 +125,6 @@ public class TokenGroupsToAttributeProcessor implements IAttributesProcessor, In
     public void setTargetAtribute(String targetAtribute) {
 
         this.targetAtribute = targetAtribute;
-    }
-
-    public boolean isRaiseIllegalFormatException() {
-
-        return raiseIllegalFormatException;
-    }
-
-    public void setRaiseIllegalFormatException(boolean raiseIllegalFormatException) {
-
-        this.raiseIllegalFormatException = raiseIllegalFormatException;
-    }
-
-    public ContextSource getContextSource() {
-
-        return contextSource;
-    }
-
-    public void setContextSource(ContextSource contextSource) {
-
-        this.contextSource = contextSource;
-    }
-
-    
-    public boolean isUseTokenGroupsAttribute() {
-    
-        return useTokenGroupsAttribute;
-    }
-
-    
-    public void setUseTokenGroupsAttribute(boolean useTokenGroupsAttribute) {
-    
-        this.useTokenGroupsAttribute = useTokenGroupsAttribute;
-    }
-
-    // Wrapper getter around LdapName
-    public String getBaseDN() {
-
-        return (contextSourceBaseDN == null ? "" : contextSourceBaseDN.toString());
-    }
-
-    public void setBaseDN(String baseDN) {
-
-        if (baseDN == null) {
-            throw new IllegalArgumentException("baseDN cannot be null");
-        }
-        try {
-            this.contextSourceBaseDN = new LdapName(baseDN);
-        } catch (InvalidNameException e) {
-            throw new IllegalArgumentException(e);
-        }
-
-    }
-
-    /**
-     * Private class which maps tokenGroups to real groups.
-     * 
-     * @author Philippe Marasse <philippe.marasse@laposte.net> *
-     */
-    private class TokenGroupMapper implements ContextMapper {
-
-        List<Object> decodedValues = new ArrayList<Object>();
-
-        @Override
-        public Object mapFromContext(Object ctx) {
-
-            DirContextAdapter context = (DirContextAdapter) ctx;
-            if (log.isDebugEnabled()) {
-                log.debug("Attributes returned by context : " + context.getAttributes().toString());
-            }
-
-            Object[] values = context.getObjectAttributes(useTokenGroupsAttribute ? TOKEN_GROUPS_ATTR : TOKEN_GROUPS_NO_GC_ATTR);
-
-            if (values != null) {
-
-                if (log.isDebugEnabled()) {
-                    log.debug("Attribute " + (useTokenGroupsAttribute ? TOKEN_GROUPS_ATTR : TOKEN_GROUPS_NO_GC_ATTR) + " Values : "
-                            + Arrays.toString(values));
-                }
-
-                for (Object rawValue : values) {
-                    String group = tokenRegistry.getDnFromToken((byte[]) rawValue);
-                    if (group != null) {
-                        decodedValues.add(group);
-                    } else {
-                        if (log.isInfoEnabled()) {
-                            log.info("unable to decode raw tokenGroup class : " + rawValue.getClass().getCanonicalName());
-                        }
-                    }
-                }
-            }
-
-            if (log.isDebugEnabled()) {
-                log.debug("Returning decoded groups : " + Arrays.toString(decodedValues.toArray()));
-            }
-            return decodedValues;
-        }
-
     }
 
 }
--- src/test/java/net/archigny/cas/persondir/adutils/TokenGroupsToAttributeProcessorTest.java
+++ src/test/java/net/archigny/cas/persondir/adutils/TokenGroupsToAttributeProcessorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import net.archigny.cas.persondir.ldap.LdapPersonAttributeDao;
@@ -20,42 +21,45 @@ import org.springframework.ldap.core.support.LdapContextSource;
 
 public class TokenGroupsToAttributeProcessorTest {
 
-    public final static Logger            log          = LoggerFactory.getLogger(TokenGroupsToAttributeProcessorTest.class);
+    public final static Logger              log                = LoggerFactory.getLogger(TokenGroupsToAttributeProcessorTest.class);
 
-    public final static String            BASE_DN      = "dc=in,dc=archigny,dc=org";
+    public final static String              BASE_DN            = "dc=in,dc=archigny,dc=org";
 
-    public final static String            BIND_DN      = "cn=Application Test,ou=Applications,ou=Utilisateurs,dc=in,dc=archigny,dc=org";
+    public final static String              BIND_DN            = "cn=Application Test,ou=Applications,ou=Utilisateurs,dc=in,dc=archigny,dc=org";
 
-    public final static String            BIND_PW      = "123456";
+    public final static String              BIND_PW            = "123456";
 
-    public final static String            SERVER_URL   = "ldap://win2k8.in.archigny.org";
+    public final static String              SERVER_URL         = "ldap://win2k8.in.archigny.org";
 
-    public final static String            LDAP_FILTER  = "(samAccountName={0})";
+    public final static String              LDAP_FILTER        = "(samAccountName={0})";
 
-    public final static String            TOKEN_GROUPS = "tokenGroups";
+    public final static String              TOKEN_GROUPS       = "tokenGroups";
 
-    public final static String            CN           = "cn";
+    public final static String              TOKEN_GROUPS_NO_GC = "tokenGroupsNoGCAcceptable";
 
-    public final static String            DN_ATTR      = "userDn";
+    public final static String              CN                 = "cn";
 
-    public final static String            TARGET_NAME  = "groups";
+    public final static String              DN_ATTR            = "userDn";
 
-    public final static String            USER1_ID     = "test1";
+    public final static String              TARGET_NAME        = "groups";
 
-    public final static String            USER2_ID     = "philippe";
+    public final static String              USER1_ID           = "test1";
 
-    private LdapPersonAttributeDao personAttributeDao;
+    public final static String              USER2_ID           = "philippe";
 
-    private LdapContextSource      ldapCS;
-    
+    private LdapPersonAttributeDao          personAttributeDao;
+
+    private LdapContextSource               ldapCS;
+
     private TokenGroupsToAttributeProcessor processor;
 
     @Before
     public void globalSetUp() throws Exception {
 
-/*        Hashtable<String, String> env = new Hashtable<String, String>();
-        env.put("java.naming.ldap.attributes.binary","tokenGroups tokenGroupsNoGCAcceptable");
-  */      
+        /*
+         * Hashtable<String, String> env = new Hashtable<String, String>();
+         * env.put("java.naming.ldap.attributes.binary","tokenGroups tokenGroupsNoGCAcceptable");
+         */
         // Crée une contextSource LDAP bean
         log.debug("Creating LdapContextSource");
         ldapCS = new LdapContextSource();
@@ -63,8 +67,12 @@ public class TokenGroupsToAttributeProcessorTest {
         ldapCS.setPooled(true);
         ldapCS.setUserDn(BIND_DN);
         ldapCS.setPassword(BIND_PW);
-        ldapCS.setUrl(SERVER_URL);       
-        //ldapCS.setBaseEnvironmentProperties(env);
+        ldapCS.setUrl(SERVER_URL);
+        
+        HashMap<String,String> env = new HashMap<String, String>();
+        env.put("java.naming.ldap.attributes.binary", TOKEN_GROUPS + " " + TOKEN_GROUPS_NO_GC);
+        ldapCS.setBaseEnvironmentProperties(env);
+        // ldapCS.setBaseEnvironmentProperties(env);
         ldapCS.afterPropertiesSet();
         log.debug("LdapContextSource created");
 
@@ -75,6 +83,8 @@ public class TokenGroupsToAttributeProcessorTest {
         List<String> queriedAttributes = new ArrayList<String>();
         // queriedAttributes.add(TOKEN_GROUPS);
         queriedAttributes.add(CN);
+        queriedAttributes.add(TOKEN_GROUPS);
+        queriedAttributes.add(TOKEN_GROUPS_NO_GC);
 
         // token registry bean
         log.debug("Creating CachingADTokenGroupsRegistry");
@@ -92,9 +102,8 @@ public class TokenGroupsToAttributeProcessorTest {
         log.debug("Creating TokenGroupsToAttributeProcessor");
         processor = new TokenGroupsToAttributeProcessor();
         processor.setTokenRegistry(tokenRegistry);
-        processor.setDnAttribute(DN_ATTR);
+        processor.setSourceAttribute(TOKEN_GROUPS);
         processor.setTargetAtribute(TARGET_NAME);
-        processor.setContextSource(ldapCS);
         processor.afterPropertiesSet();
         log.debug("TokenGroupsToAttributeProcessor created");
 
@@ -107,6 +116,7 @@ public class TokenGroupsToAttributeProcessorTest {
         personAttributeDao.setProcessors(processors);
         personAttributeDao.setLdapFilter(LDAP_FILTER);
         personAttributeDao.setDnAttributeName(DN_ATTR);
+        personAttributeDao.setFetchDirectDn(true);
         personAttributeDao.afterPropertiesSet();
         log.debug("LdapPersonAttributeDao created");
 
@@ -121,7 +131,7 @@ public class TokenGroupsToAttributeProcessorTest {
         IPersonAttributes attributes = personAttributeDao.getPerson(USER1_ID);
         List<Object> values = attributes.getAttributeValues(TARGET_NAME);
         log.debug("retrieved target attributes : " + Arrays.toString(values.toArray()));
-        
+
         // User1 member of cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org
         // and cn=Utilisateurs du domaine,cn=Users,dc=in,dc=archigny,dc=org]
 
@@ -129,27 +139,25 @@ public class TokenGroupsToAttributeProcessorTest {
         assertEquals(2, values.size());
 
         HashSet<DistinguishedName> currentGroups = new HashSet<DistinguishedName>(2);
-        
+
         for (Object object : values) {
             currentGroups.add(new DistinguishedName((String) object));
         }
-        
+
         DistinguishedName expectedGroup1 = new DistinguishedName("cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org");
         DistinguishedName expectedGroup2 = new DistinguishedName("cn=Utilisateurs du domaine,cn=Users,dc=in,dc=archigny,dc=org");
-        
+
         assertTrue(currentGroups.contains(expectedGroup1));
         assertTrue(currentGroups.contains(expectedGroup2));
-        
+
         attributes = personAttributeDao.getPerson(USER2_ID);
         values = attributes.getAttributeValues(TARGET_NAME);
         log.debug("retrieved target attributes : " + Arrays.toString(values.toArray()));
-        
-        /* Groups expected : 
-         * cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org, 
-         * cn=Groupe local indirect,ou=Groupes,dc=in,dc=archigny,dc=org, 
-         * cn=groupe indirect,ou=Groupes,dc=in,dc=archigny,dc=org, 
-         * cn=Utilisateurs du domaine,cn=Users,dc=in,dc=archigny,dc=org, 
-         * cn=groupe direct,ou=Groupes,dc=in,dc=archigny,dc=org]
+
+        /*
+         * Groups expected : cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org, cn=Groupe local
+         * indirect,ou=Groupes,dc=in,dc=archigny,dc=org, cn=groupe indirect,ou=Groupes,dc=in,dc=archigny,dc=org, cn=Utilisateurs du
+         * domaine,cn=Users,dc=in,dc=archigny,dc=org, cn=groupe direct,ou=Groupes,dc=in,dc=archigny,dc=org]
          */
 
         assertEquals(5, values.size());
@@ -161,13 +169,13 @@ public class TokenGroupsToAttributeProcessorTest {
         DistinguishedName expectedGroup3 = new DistinguishedName("cn=Groupe local indirect,ou=Groupes,dc=in,dc=archigny,dc=org");
         DistinguishedName expectedGroup4 = new DistinguishedName("cn=groupe indirect,ou=Groupes,dc=in,dc=archigny,dc=org");
         DistinguishedName expectedGroup5 = new DistinguishedName("cn=groupe direct,ou=Groupes,dc=in,dc=archigny,dc=org");
-        
+
         assertTrue(currentGroups.contains(expectedGroup1));
         assertTrue(currentGroups.contains(expectedGroup2));
         assertTrue(currentGroups.contains(expectedGroup3));
         assertTrue(currentGroups.contains(expectedGroup4));
         assertTrue(currentGroups.contains(expectedGroup5));
-        
+
         log.info("RealTest end.");
     }
 
@@ -175,12 +183,12 @@ public class TokenGroupsToAttributeProcessorTest {
     public void RealTestNoGC() {
 
         log.debug("RealTestNoGC : same test but using tokenGroupsNoGCAcceptable instead of tokenGroups attribute");
-        processor.setUseTokenGroupsAttribute(false);
-        
+        processor.setSourceAttribute(TOKEN_GROUPS_NO_GC);
+
         IPersonAttributes attributes = personAttributeDao.getPerson(USER1_ID);
         List<Object> values = attributes.getAttributeValues(TARGET_NAME);
         log.debug("retrieved target attributes : " + Arrays.toString(values.toArray()));
-        
+
         // User1 member of cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org
         // and cn=Utilisateurs du domaine,cn=Users,dc=in,dc=archigny,dc=org]
 
@@ -188,27 +196,25 @@ public class TokenGroupsToAttributeProcessorTest {
         assertEquals(2, values.size());
 
         HashSet<DistinguishedName> currentGroups = new HashSet<DistinguishedName>(2);
-        
+
         for (Object object : values) {
             currentGroups.add(new DistinguishedName((String) object));
         }
-        
+
         DistinguishedName expectedGroup1 = new DistinguishedName("cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org");
         DistinguishedName expectedGroup2 = new DistinguishedName("cn=Utilisateurs du domaine,cn=Users,dc=in,dc=archigny,dc=org");
-        
+
         assertTrue(currentGroups.contains(expectedGroup1));
         assertTrue(currentGroups.contains(expectedGroup2));
-        
+
         attributes = personAttributeDao.getPerson(USER2_ID);
         values = attributes.getAttributeValues(TARGET_NAME);
         log.debug("retrieved target attributes : " + Arrays.toString(values.toArray()));
-        
-        /* Groups expected : 
-         * cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org, 
-         * cn=Groupe local indirect,ou=Groupes,dc=in,dc=archigny,dc=org, 
-         * cn=groupe indirect,ou=Groupes,dc=in,dc=archigny,dc=org, 
-         * cn=Utilisateurs du domaine,cn=Users,dc=in,dc=archigny,dc=org, 
-         * cn=groupe direct,ou=Groupes,dc=in,dc=archigny,dc=org]
+
+        /*
+         * Groups expected : cn=Utilisateurs,cn=Builtin,dc=in,dc=archigny,dc=org, cn=Groupe local
+         * indirect,ou=Groupes,dc=in,dc=archigny,dc=org, cn=groupe indirect,ou=Groupes,dc=in,dc=archigny,dc=org, cn=Utilisateurs du
+         * domaine,cn=Users,dc=in,dc=archigny,dc=org, cn=groupe direct,ou=Groupes,dc=in,dc=archigny,dc=org]
          */
 
         assertEquals(5, values.size());
@@ -220,15 +226,13 @@ public class TokenGroupsToAttributeProcessorTest {
         DistinguishedName expectedGroup3 = new DistinguishedName("cn=Groupe local indirect,ou=Groupes,dc=in,dc=archigny,dc=org");
         DistinguishedName expectedGroup4 = new DistinguishedName("cn=groupe indirect,ou=Groupes,dc=in,dc=archigny,dc=org");
         DistinguishedName expectedGroup5 = new DistinguishedName("cn=groupe direct,ou=Groupes,dc=in,dc=archigny,dc=org");
-        
+
         assertTrue(currentGroups.contains(expectedGroup1));
         assertTrue(currentGroups.contains(expectedGroup2));
         assertTrue(currentGroups.contains(expectedGroup3));
         assertTrue(currentGroups.contains(expectedGroup4));
         assertTrue(currentGroups.contains(expectedGroup5));
-        
-        
+
     }
 
-    
 }
