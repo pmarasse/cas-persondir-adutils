2edc8c98a9f7cf77ccf12d121a015cc73cf2df21
Philippe MARASSE <philippe.marasse@ch-poitiers.fr>
2012-08-13 10:25:04 +0200

    Removed unused imports

--- src/test/java/net/archigny/cas/persondir/adutils/ByteArrayToSIDProcessorTest.java
+++ src/test/java/net/archigny/cas/persondir/adutils/ByteArrayToSIDProcessorTest.java
@@ -3,7 +3,6 @@ package net.archigny.cas.persondir.adutils;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
